9c0e852ac5b8d04eabba3ecaf3a27965aa44f12d
zhangwenhua <cszhangwenhua@gamil.com>
2016-05-26 11:10:43 +0800

    test
    test

---
+++ java-test/src/main/java/com/fantasy/training/test/Firsttest.java
@@ -0,0 +1,11 @@
+package com.fantasy.training.test;
+
+/**
+ * Created by Administrator on 2016/5/26.
+ */
+public class Firsttest {
+    public static void main(String[] args){
+        System.out.println("Firsttest");
+    }
+
+}
